059953b4ff2f418a4320ef0fb2c960e137c8297b
vivian <vivian.v18723@gmail.com>
2022-08-15 12:08:55 +0100

    separate js variables & function  into own files

--- static/scripts/universal.js
+++ static/scripts/universal.js
@@ -1,14 +1,4 @@
+// ALL VARIABLES
 const navBarSmallBg = document.getElementById("navBarSmallBg");
 const navLinksSmall = document.getElementById("navLinksSmall");
 const navBarIcon = document.getElementById("bars");
-
-// toggles nav width
-navBarIcon.addEventListener("click", () => {
-  if (navBarSmallBg.style.height === "4rem") {
-    navBarSmallBg.style.height = "10rem";
-    navLinksSmall.style.display = "grid";
-  } else {
-    navBarSmallBg.style.height = "4rem";
-    navLinksSmall.style.display = "none";
-  }
-});

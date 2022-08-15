7302ba4b693bee84c998d76885fa2d84bae740f2
vivian <vivian.v18723@gmail.com>
2022-08-15 19:42:46 +0100

    move all js to single file

---
+++ static/scripts/scripts.js
@@ -0,0 +1,16 @@
+// ALL VARIABLES
+const navBarSmallBg = document.getElementById("navBarSmallBg");
+const navLinksSmall = document.getElementById("navLinksSmall");
+const navBarIcon = document.getElementById("bars");
+
+// toggles nav width & links appearance
+// navBarIcon.addEventListener("click", () => {
+//   if (navBarSmallBg.style.height === "4rem") {
+//     navBarSmallBg.style.height = "10rem";
+//     navLinksSmall.style.display = "grid";
+//   } else {
+//     navBarSmallBg.style.height = "4rem";
+//     navLinksSmall.style.display = "none";
+//   }
+// });
+
